ba375c5b10b1e98bfcb0c7645b4d9fad3c3fda3c
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-01 15:55:25 +0200

    Resolving tasks

--- 01. JAVASCRIPT SYNTAX/Next Day.js
+++ 01. JAVASCRIPT SYNTAX/Next Day.js
@@ -1,7 +1,9 @@
 function nextDay(year, month, day) {
-    let date = new Date(year, month, day);
+    let date = new Date(year, month - 1, day);
+    let oneDay = 24 * 60 * 60 * 1000;
+    let nextDay = new Date(date.getTime() + oneDay);
 
-    console.log(date);
+    return nextDay.getFullYear() + "-" + (nextDay.getMonth() + 1) + "-" + nextDay.getDate();
 }
 
 nextDay(2016, 9, 30);
