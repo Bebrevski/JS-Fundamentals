bc19e5bb719fba238b854d3798983591dfc46b39
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-05 12:23:10 +0200

    Course exercises

--- 04. EXERCISE FUNCTIONS/04. Cooking by Numbers.js
+++ 04. EXERCISE FUNCTIONS/04. Cooking by Numbers.js
@@ -1,35 +1,24 @@
 function cookTheNumber(tokens) {
 
-    let startingNum = Number(tokens[0]);
+    let [start, op1, op2, op3, op4, op5] =
+    [Number(tokens[0]), tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]];
 
-    doOperations(startingNum, tokens);
+    let chop = (num) => num / 2;
+    let dice = (num) => Math.sqrt(num);
+    let spice = (num) => ++num;
+    let bake = (num) => num * 3;
+    let fillet = (num) => num * 0.8;
 
-    function doOperations(num, arr) {
-
-        let result = num;
-
-        for (let i = 1; i < arr.length; i++) {
-            switch (arr[i]){
-                case 'chop':
-                    console.log(result /= 2);
-                    break;
-                case 'dice':
-                    console.log(result = Math.sqrt(result));
-                    break;
-                case 'spice':
-                    console.log(++result);
-                    break;
-                case 'bake':
-                    console.log(result *= 3);
-                    break;
-                case 'fillet':
-                    console.log(result *= 0.8);
-                    break;
-                default: break;
-            }
+    for (let op of [op1, op2, op3, op4, op5]) {
+        switch (op){
+            case 'chop': start = chop(start); break;
+            case 'dice': start = dice(start); break;
+            case 'spice': start = spice(start); break;
+            case 'bake': start = bake(start); break;
+            case 'fillet': start = fillet(start); break;
         }
 
-        return result
+        console.log(start);
     }
 }
 
