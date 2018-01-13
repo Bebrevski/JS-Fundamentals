3d9fd03275e13ef13bb794355aa200570149b45c
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-13 16:07:44 +0200

    Course exercises

---
+++ 06. EXERCISE ARRAYS AND MATRICES/10. Orbit (2 Stars).js
@@ -0,0 +1,80 @@
+function solve(data) {
+
+    let width = data[0];
+    let height = data[1];
+    let x = data[2];
+    let y = data[3];
+
+    let matrix = [];
+    for (let i = 0; i < height; i++) {
+        let arr = [];
+        for (let j = 0; j < width; j++) {
+            arr.push(0);
+        }
+        matrix.push(arr);
+    }
+
+    matrix[x][y] = 1;
+
+    let number = 2;
+    let rowStart = 0;
+    let rowEnd = x;
+    let colStart = 0;
+    let colEnd = y;
+
+    let counterRowEnd = rowEnd;
+    let counterColEnd = colEnd;
+    let counterRowStart = rowEnd;
+    let counterColStart = colEnd;
+
+    while (true) {
+
+        counterRowEnd++;
+        counterColEnd++;
+        counterRowStart--;
+        counterColStart--;
+
+        if (counterRowEnd > width - 1 && counterColEnd > height - 1 && counterRowStart < 0 && counterColStart < 0) {
+            break;
+        }
+        if (rowEnd >= width - 1) {
+            rowEnd = height - 1;
+        }
+        else {
+            rowEnd++;
+        }
+        if (colEnd >= width - 1) {
+            colEnd = height - 1;
+        }
+        else {
+            colEnd++;
+        }
+        if (rowStart === 0) {
+            rowStart = 0;
+        }
+        else {
+            rowStart--;
+        }
+        if (colStart === 0) {
+            colStart = 0;
+        }
+        else {
+            colStart--;
+        }
+
+        for (let i = rowStart; i <= rowEnd; i++) {
+            for (let j = colStart; j <= colEnd; j++) {
+                if (matrix[i][j] === 0) {
+                    matrix[i][j] = number;
+                }
+            }
+        }
+        number++;
+    }
+
+    console.log(matrix.map(row => row.join(' ')).join('\n') + '\n');
+}
+
+solve([4, 4, 0, 0]);
+solve([5, 5, 2, 2]);
+solve([3, 3, 2, 2]);
