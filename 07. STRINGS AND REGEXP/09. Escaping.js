e1b824fb2ac473caff6b4a0c903c2c58cb638f67
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-15 18:55:16 +0200

    Course exercises: end of lab

--- 07. STRINGS AND REGEXP/09. Escaping.js
+++ 07. STRINGS AND REGEXP/09. Escaping.js
@@ -3,45 +3,26 @@ function htmlEscape(arr) {
     let result = '<ul>\n';
 
     for (let i = 0; i < arr.length; i++) {
-        result += ' <li>';
 
-        let currentText = arr[i];
-        currentText = replace(currentText, '&');
-        currentText = replace(currentText, '>');
-        currentText = replace(currentText, '<');
-        currentText = replace(currentText, '\"');
+        result += '  <li>';
 
-        result += `${currentText}</li>\n`;
+        let text = arr[i]
+            .replace(/&/gm, '&amp;')
+            .replace(/</gm, '&lt;')
+            .replace(/>/gm, '&gt;')
+            .replace(/"/gm, '&quot;');
+
+
+        result += `${text}</li>\n`;
     }
 
     result += '</ul>';
 
     console.log(result);
-
-    function replace(text, symbol) {
-
-        let current = text;
-        let index = current.indexOf(symbol);
-
-        while (index !== -1) {
-            if (symbol === '<') {
-                current = current.replace(symbol, '&lt;');
-                index = current.indexOf(symbol);
-            } else if (symbol === '>') {
-                current = current.replace(symbol, '&gt;');
-                index = current.indexOf(symbol);
-            } else if (symbol === '&') {
-                current = current.replace(symbol, '&amp;');
-                index = current.indexOf(symbol);
-            } else if (symbol === '\"') {
-                current = current.replace(symbol, '&quot;');
-                index = current.indexOf(symbol);
-            }
-        }
-
-        return current;
-    }
 }
+
+htmlEscape(['<b>unescaped text</b>', 'normal text']);
+
 
 /*
 
