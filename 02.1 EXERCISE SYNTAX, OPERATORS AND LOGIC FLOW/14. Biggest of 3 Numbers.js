52b63d0c97484248378f7c02cdef065c1c2a2501
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-19 18:08:52 +0200

    Course Exercises

---
+++ 02.1 EXERCISE SYNTAX, OPERATORS AND LOGIC FLOW/14. Biggest of 3 Numbers.js
@@ -0,0 +1,3 @@
+function solve([num1, num2, num3]) {
+    return Math.max(num1, Math.max(num2, num3));
+}
---
+++ 02.1 EXERCISE SYNTAX, OPERATORS AND LOGIC FLOW/15. Point in Rectangle.js
@@ -0,0 +1,4 @@
+function solve([x, y, xMin, xMax, yMin, yMax]) {
+
+    return x >= xMin && x <= xMax && y >= yMin && y <= yMax ? 'inside' : 'outside';
+}
