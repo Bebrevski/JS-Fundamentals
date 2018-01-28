548bea6014274ef7e6a3cc17e130cda88ef29323
Bebrevski <stefan.bebrevski89@gmail.com>
2018-01-28 13:11:51 +0200

    exam preparations

--- 11. Exam Preps/JS Fundamentals Exam - 16 December 2016/Problem 4 – Airport Statistics.js
+++ 11. Exam Preps/JS Fundamentals Exam - 16 December 2016/Problem 4 – Airport Statistics.js
@@ -19,7 +19,12 @@ function solve(recordOfPlanes) {
         }
 
         if (!townAndPassengers.has(town)) {
-            townAndPassengers.set(town, {arrivals: Number(passengers), departures: 0});
+            if (planesLanded.get(plane) === 'land') {
+                townAndPassengers.set(town, {arrivals: Number(passengers), departures: 0});
+            } else if (planesLanded.get(plane) === 'depart') {
+                townAndPassengers.set(town, {arrivals: 0, departures: Number(passengers)});
+            }
+
         } else {
             if (planesLanded.get(plane) === 'land') {
                 let arrivalsOldValue = townAndPassengers.get(town).arrivals;
@@ -46,20 +51,24 @@ function solve(recordOfPlanes) {
         planeAndCity.get(plane).add(town);
     }
 
-    console.log([...planesLanded]);
-
     console.log('Planes left:');
-    for (let [plane, condition] of planesLanded) {
+    for (let [plane, condition] of [...planesLanded].sort((p1, p2) => p1[0].localeCompare(p2[0]))) {
         if (condition === 'land') {
             console.log(`- ${plane}`);
         }
     }
-    for (let [town, passengers] of townAndPassengers) {
+    for (let [town, passengers] of [...townAndPassengers].sort(function (a, b) {
+        if (b[1].arrivals > a[1].arrivals) return 1;
+        if (b[1].arrivals < a[1].arrivals) return -1;
+        return a[0].localeCompare(b[0]);
+    })) {
         console.log(town);
         console.log(`Arrivals: ${passengers.arrivals}`);
         console.log(`Departures: ${passengers.departures}`);
         console.log('Planes:');
-        for (let [plane, city] of planeAndCity) {
+        for (let [plane, city] of [...planeAndCity].sort(function (a, b) {
+            return a[0].toLowerCase().localeCompare(b[0].toLowerCase());
+        })) {
             if (city.has(town)) {
                 console.log(`-- ${plane}`);
             }
@@ -67,17 +76,58 @@ function solve(recordOfPlanes) {
     }
 }
 
-solve([
-        "Boeing474 Madrid 300 land",
-        "AirForceOne WashingtonDC 178 land",
-        "Airbus London 265 depart",
-        "ATR72 WashingtonDC 272 land",
-        "ATR72 Madrid 135 depart"
-    ]
-);
+/*
+function solve(input) {
+    let planes = new Set();
+    let port = new Map();
 
-solve(["Airbus Paris 356 land",
-    "Airbus London 321 land",
-    "Airbus Paris 213 depart",
-    "Airbus Ljubljana 250 land"
-]);
+    for (let plane of input) {
+        let tokens = plane.split(" ");
+        let name = tokens[0];
+        let town = tokens[1];
+        let ppl = Number(tokens[2]);
+        let action = tokens[3];
+
+        if (action == 'depart') {
+            if (!planes.has(name)) continue;
+            else {
+                planes.delete(name);
+            }
+        }
+        if (action == 'land') {
+            if (planes.has(name)) continue;
+            else {
+                planes.add(name);
+            }
+        }
+
+        if (!port.has(town)) {
+            port.set(town, {planes: [], arrivals: 0, departures: 0});
+        }
+        if (!port.get(town).planes.includes(name)) {
+            port.get(town).planes.push(name);
+        }
+        if (action == "land") {
+            port.get(town).arrivals += ppl;
+        } else {
+            port.get(town).departures += ppl;
+        }
+    }
+    console.log("Planes left:");
+    [...planes].sort((p1, p2) => p1.localeCompare(p2)).forEach(p => console.log(`- ${p}`));
+    [...port].sort((t1, t2) => {
+        if (t1[1].arrivals < t2[1].arrivals) return 1;
+        if (t1[1].arrivals > t2[1].arrivals) return -1;
+        return t1[0].localeCompare(t2[0]);
+    }).forEach(logData);
+
+    function logData(town) {
+        //console.log(`${town[0]} ${town[1].arrivals} ${town[1].departures}`);
+        console.log(town[0]);
+        console.log(`Arrivals: ${town[1].arrivals}`);
+        console.log(`Departures: ${town[1].departures}`);
+        console.log("Planes:");
+        town[1].planes.sort((p1, p2) => p1.localeCompare(p2)).forEach(p => console.log(`-- ${p}`));
+    }
+}
+ */
